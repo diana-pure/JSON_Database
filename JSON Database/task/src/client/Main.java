c7566f0ca0bb700542b5036c02741221e3820ec9
Diana <diana.anisutina@gmail.com>
2021-03-26 15:57:28 +0700

    +getting request from file ~refactoring

--- JSON Database/task/src/client/Main.java
+++ JSON Database/task/src/client/Main.java
@@ -1,27 +1,16 @@
 package client;
 
-import com.beust.jcommander.JCommander;
-import com.beust.jcommander.Parameter;
+import java.io.FileNotFoundException;
 
 public class Main {
     private static final String ADDRESS = "127.0.0.1";
     private static final int PORT = 23456;
 
-    @Parameter(names = {"-t"})
-    String type;
-    @Parameter(names = {"-k"})
-    String index;
-    @Parameter(names = {"-v"})
-    String value;
-
     public static void main(String[] args) {
-        Main main = new Main();
-        JCommander.newBuilder()
-                .addObject(main)
-                .build()
-                .parse(args);
-
-        String request = RequestBuilder.build(main.type, main.index, main.value);
-        new DatabaseConnector(ADDRESS, PORT).perform(request);
+        try {
+            new DatabaseConnector(ADDRESS, PORT).perform(RequestBuilder.build(args));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
--- JSON Database/task/src/client/RequestBuilder.java
+++ JSON Database/task/src/client/RequestBuilder.java
@@ -1,12 +1,38 @@
 package client;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
 import com.google.gson.Gson;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class RequestBuilder {
-    public static String build(String type, String index, String value) {
+    private static final String requestURI = "JSON Database\\task\\src\\client\\data\\";
+    @Parameter(names = {"-in"})
+    String file;
+    @Parameter(names = {"-t"})
+    String type;
+    @Parameter(names = {"-k"})
+    String index;
+    @Parameter(names = {"-v"})
+    String value;
+
+    public static String build(String[] args) throws FileNotFoundException {
+        RequestBuilder builder = new RequestBuilder();
+        JCommander.newBuilder().addObject(builder).build().parse(args);
+
+        return builder.file == null ? build(builder.type, builder.index, builder.value) : build(builder.file);
+    }
+
+    private static String build(String file) throws FileNotFoundException {
+        return new Scanner(new FileInputStream(requestURI + file)).nextLine();
+    }
+
+    private static String build(String type, String index, String value) {
         Map<String, String> requestData = new HashMap<>();
         requestData.put("type", type);
         switch (type) {
